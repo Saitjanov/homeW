83df1c12fdd5d0d285ce5e92460053bbaa5faf1d
Saitjanov <saitjanovtimur@gmail.com>
2022-09-24 16:16:11 +0500

    first commit

---
+++ homework4.js
@@ -0,0 +1,29 @@
+let month = +prompt('Напиши число я тебе сказуже мецяц?')
+
+if(month === 1) {
+    alert('Январь')
+} else if(month === 2) {
+    alert('Февраль')
+}  else if(month === 3) {
+    alert('Март')
+} else if(month === 4) {
+    alert('Апрель')
+} else if(month === 5) {
+    alert('Май')
+} else if(month === 6) {
+    alert('Июнь')
+} else if(month === 7) {
+    alert('Июль')
+} else if(month === 8) {
+    alert('Август')
+} else if(month === 9) {
+    alert('Сентябрь ')
+} else if(month === 10) {
+    alert('Октябрь')
+} else if(month === 11) {
+    alert('Ноябрь')
+} else if(month === 12) {
+    alert('Декабрь')
+} else{
+    alert('Ты не написал число')
+}
